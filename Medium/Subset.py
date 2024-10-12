068afd6c8b1c6fbafe3cec366448045e60abe41f
Youssef ElNahas <4311779@upm.edu.sa>
2024-10-12 22:03:55 +0300

    Create Subset.py

---
+++ Medium/Subset.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def backtrack(start, path):
+            result.append(path[:])
+
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+        backtrack(0, [])
+        return result
+
+sol = Solution()
+print(sol.subsets([1, 2, 3]))
