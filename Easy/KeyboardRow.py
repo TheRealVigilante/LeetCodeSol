6c3039b393cf5ad5339d11efaed9d6d922d343c3
Youssef ElNahas <4311779@upm.edu.sa>
2024-10-31 09:32:02 +0300

    Update KeyboardRow.py

--- Easy/KeyboardRow.py
+++ Easy/KeyboardRow.py
@@ -1,13 +1,18 @@
 from typing import List
 
+from scipy.special import result
+
+
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
         rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+        result = []
         for word in words:
             for row in rows:
                 if all([c.lower() in row for c in word]):
-                    yield word
-                    break
+                    result.append(word)
+
+        return result
 
 
 sol = Solution()
